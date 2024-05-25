72aac04cc0b96fad6a21d314dfe0ecd545e60447
bbazwalt <kprazwaben@gmail.com>
2024-05-26 01:15:10 +0530

    updated db properties.

--- project/todos/urls.py
+++ project/todos/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .views import TodoViewSet
 
 router = routers.DefaultRouter()
-router.register(r'api/todos', TodoViewSet, 'todos')
+router.register(r"api/todos", TodoViewSet, "todos")
 
 urlpatterns = router.urls
